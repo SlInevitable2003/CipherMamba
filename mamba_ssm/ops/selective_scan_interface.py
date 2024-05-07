cd27755de2d1c7f1854adf3da9045f0a0fef3410
slinevitable2003 <slmozart2003@gmail.com>
2024-05-07 08:20:20 -0400

    Correct to run on my PC

--- mamba_ssm/ops/selective_scan_interface.py
+++ mamba_ssm/ops/selective_scan_interface.py
@@ -13,7 +13,7 @@ except ImportError:
     causal_conv1d_fn = None
     causal_conv1d_cuda = None
 
-import selective_scan_cuda
+# import selective_scan_cuda
 
 
 class SelectiveScanFn(torch.autograd.Function):
@@ -85,7 +85,7 @@ def selective_scan_fn(u, delta, A, B, C, D=None, z=None, delta_bias=None, delta_
     last_state has shape (batch, dim, dstate). Note that the gradient of the last state is
     not considered in the backward pass.
     """
-    return SelectiveScanFn.apply(u, delta, A, B, C, D, z, delta_bias, delta_softplus, return_last_state)
+    return selective_scan_ref(u, delta, A, B, C, D, z, delta_bias, delta_softplus, return_last_state)
 
 
 def selective_scan_ref(u, delta, A, B, C, D=None, z=None, delta_bias=None, delta_softplus=False,
@@ -314,9 +314,9 @@ def mamba_inner_fn(
     A, B=None, C=None, D=None, delta_bias=None, B_proj_bias=None,
     C_proj_bias=None, delta_softplus=True
 ):
-    return MambaInnerFn.apply(xz, conv1d_weight, conv1d_bias, x_proj_weight, delta_proj_weight,
-                              out_proj_weight, out_proj_bias,
-                              A, B, C, D, delta_bias, B_proj_bias, C_proj_bias, delta_softplus)
+    return mamba_inner_ref(xz, conv1d_weight, conv1d_bias, x_proj_weight, delta_proj_weight,
+                           out_proj_weight, out_proj_bias,
+                           A, B, C, D, delta_bias, B_proj_bias, C_proj_bias, delta_softplus)
 
 
 def mamba_inner_ref(
